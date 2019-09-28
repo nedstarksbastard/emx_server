0a6009a7bd8f4251b1702f94d1fc9c83cf16711b
Fizi Yadav <fizi@outlook.com>
2019-09-28 13:54:14 -0400

    change response

--- emx_server.py
+++ emx_server.py
@@ -1,11 +1,21 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return Response("OK", status=200, mimetype='text/plain')
+
+    answer = {
+        "Ping": "OK",
+        "Phone": "6463543951",
+        "Degree": "Master of Science",
+        "Position": "Data Pipeline Engineer",
+        "Name": "Fizi Yadav"
+    }
+
+    question = request.args.get("q").title()
+    return Response(answer[question], status=200, mimetype='text/plain')
 
 
 if __name__ == "__main__":
