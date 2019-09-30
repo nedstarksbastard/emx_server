c1d17a8b43b6e5986c09c91bd87e7439a6f11d96
Fizi Yadav <fizi@outlook.com>
2019-09-29 20:00:28 -0400

    add comments to graph class

--- utils/dag.py
+++ utils/dag.py
@@ -3,10 +3,14 @@ from collections import defaultdict, deque
 
 class Graph:
     def __init__(self):
+        """Constructor that initiates a defaultdict holding the graph"""
+
         self.graph = defaultdict(list)
 
     @property
     def nodes(self):
+        """Property returning all the nodes in a graph as a list"""
+
         nodes = set()
         nodes.update(self.graph.keys())
         for node in self.graph.keys():
@@ -15,11 +19,19 @@ class Graph:
         return list(nodes)
 
     def add_node(self, node):
+        """
+        Add a node to the graph
+        Args:
+            node: int,str
+        """
+
         if node not in self.nodes:
             self.graph[node] = list()
 
     @property
     def edges(self):
+        """Property returning all the edges in a graph as a list"""
+
         edges = list()
         for source, neighbors in self.graph.items():
             for neighbor in neighbors:
@@ -27,10 +39,16 @@ class Graph:
         return edges
 
     def add_edge(self, edge):
+        """
+        Add an edge to the graph. An edge is passed as atupe
+        Args:
+            edge: tuple, list
+        """
         node1, node2 = edge
         self.graph[node1].append(node2)
 
     def topological_util(self, node, visited, label):
+        """Recursive utility function to perform dfs on the graph"""
         visited[node] = True
         for edge in self.graph[node]:
             if not visited[edge]:
@@ -38,6 +56,8 @@ class Graph:
         label.appendleft(node)
 
     def topological_sort(self):
+        """Sort the graph according to its edges"""
+
         visited = dict.fromkeys(self.nodes, False)
         # store all nodes in topological order, the index is the position
         label = deque()
